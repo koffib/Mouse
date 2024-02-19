545d15c6731d9fe96664859593d963036eb08696
koffib <kbecanty@student.rtc.edu>
2024-02-18 19:44:15 -0800

    projet terminer

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
+
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
+
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
+// });
+
+const mouses = document.querySelectorAll(".mouse");
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
+});
+
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
 
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
